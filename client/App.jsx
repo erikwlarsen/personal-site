5ca7cf8b9615e458a5d26bcfd2fe72ec735137ef
erikwlarsen <erik.w.larsen@gmail.com>
2018-06-14 21:07:12 -0400

    (create) add spinning globe scene - drag is buggy

--- client/App.jsx
+++ client/App.jsx
@@ -1,10 +1,12 @@
 import React, { Component } from 'react';
+import Scene from './Scene.jsx';
 
 class App extends Component {
   render() {
     return (
       <div>
         <h1>SICK APP</h1>
+        <Scene />
       </div>
     );
   }
---
+++ client/Scene.jsx
@@ -0,0 +1,121 @@
+import React, { Component } from 'react';
+import * as THREE from 'three';
+
+class Scene extends Component {
+  constructor(props) {
+    super(props);
+
+    this.moving = true;
+    this.start = this.start.bind(this);
+    this.stop = this.stop.bind(this);
+    this.animate = this.animate.bind(this);
+    this.handleDrag = this.handleDrag.bind(this);
+    this.handleDragEnd = this.handleDragEnd.bind(this);
+  }
+
+  componentDidMount() {
+    this.renderer = new THREE.WebGLRenderer({ antialias: true });
+    const width = this.mount.clientWidth;
+    this.lastX = width / 2;
+    const height = this.mount.clientHeight;
+    this.lastY = height / 2;
+    this.renderer.setSize(width, height);
+    this.renderer.autoClear = false;
+
+    // set camera attributes
+    const viewAngle = 45;
+    const aspect = width / height;
+    const near = 0.1;
+    const far = 10000;
+
+    this.camera = new THREE.PerspectiveCamera(viewAngle, aspect, near, far);
+    this.camera.position.set(0, 0, 500);
+
+    this.scene = new THREE.Scene();
+    this.scene.background = new THREE.Color(0x000);
+
+    this.scene.add(this.camera);
+
+    // GLOBE
+    // set attributes
+    const radius = 200;
+    const segments = 50;
+    const rings = 50;
+    
+    this.globe = new THREE.Group();
+    this.scene.add(this.globe);
+
+    const loader = new THREE.TextureLoader();
+    const imageUris = ['earth_bright_blue.jpg', 'earthtruecolor_nasa_big.jpg', 'nasa_earth.jpg', 'simple_earth.png'];
+    const imageIdx = 1;
+    const imgLink = 'assets/images/' + imageUris[imageIdx];
+    loader.load(imgLink, (texture) => {
+      const sphere = new THREE.SphereGeometry(radius, segments, rings, 10);
+      const material = new THREE.MeshBasicMaterial({ map: texture, overdraw: 0.5 });
+      const mesh = new THREE.Mesh(sphere, material);
+      this.globe.add(mesh);
+    });
+
+    this.globe.position.z = -300;
+    // set initial offset angle. is there a better way to do this? probably
+    this.globe.rotation.x -= 0.2;
+
+    // add renderer DOM element to mount ref -- will appear when componentDidMount is called
+    this.mount.appendChild(this.renderer.domElement);
+
+    this.start();
+  }
+
+  componentWillUnmount() {
+    this.stop();
+    this.mount.removeChild(this.renderer.domElement);
+  }
+
+  start() {
+    if (!this.frameId) this.frameId = requestAnimationFrame(this.animate);
+  }
+
+  stop() {
+    cancelAnimationFrame(this.frameId);
+  }
+
+  animate() {
+    // this.globe.rotation.x += 0.003;
+    if (this.moving) this.move();
+    this.renderer.render(this.scene, this.camera);
+    requestAnimationFrame(this.animate);
+  }
+
+  move() {
+    this.globe.rotation.y += 0.003;
+  }
+
+  handleDrag(e) {
+    e.preventDefault();
+    this.moving = false;
+    const xMove = e.clientX - this.lastX;
+    const yMove = e.clientY - this.lastY;
+    this.globe.rotation.y += xMove * 0.003;
+    this.globe.rotation.x += yMove * 0.003;
+    this.lastX = e.clientX;
+    this.lastY = e.clientY;
+  }
+
+  handleDragEnd() {
+    this.moving = true;
+  }
+
+  render() {
+    return (
+      <div
+        style={{ width: '1000px', height: '1000px' }}
+        ref={(mount) => this.mount = mount}
+        onDrag={this.handleDrag}
+        onDragEnd={this.handleDragEnd}
+        draggable={true}
+      />
+    );
+  }
+}
+
+export default Scene;
