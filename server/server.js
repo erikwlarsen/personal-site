4f2834bbdd9b292d266e0683125b753b786d8a3a
erikwlarsen <erik.w.larsen@gmail.com>
2018-06-18 00:02:53 -0400

    (update) minor change to static directories

--- server/server.js
+++ server/server.js
@@ -4,7 +4,8 @@ const path = require('path');
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-app.use(express.static(path.resolve(__dirname, '../dist')));
+app.use(express.static(path.resolve(__dirname, '../dist/assets')));
+app.use(express.static(path.resolve(__dirname, '../dist/build')));
 
 app.get('/', (req, res) => {
   res.sendFile(path.resolve(__dirname, '../index.html'));
