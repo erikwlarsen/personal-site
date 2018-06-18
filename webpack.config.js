e538324e32d6262b5f5d432429f7d3a30fe5d3bc
erikwlarsen <erik.w.larsen@gmail.com>
2018-06-18 00:03:26 -0400

    (update) configure for cesium

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,9 @@
 const path = require('path');
+const CopyWebpackPlugin = require('copy-webpack-plugin');
+const webpack = require('webpack');
+
+const cesiumSource = 'node_modules/cesium/Source';
+const cesiumWorkers = '../Build/Cesium/Workers';
 
 module.exports = {
   context: __dirname,
@@ -6,6 +11,16 @@ module.exports = {
   output: {
     path: path.resolve(__dirname, 'dist/build'),
     filename: 'bundle.js',
+    // Needed to compile multiline strings in Cesium
+    sourcePrefix: '',
+  },
+  amd: {
+    // Enable webpack-friendly use of require in Cesium
+    toUrlUndefined: true,
+  },
+  node: {
+    // Resolve node module use of fs
+    fs: 'empty',
   },
   module: {
     rules: [
@@ -13,7 +28,30 @@ module.exports = {
         test: /\.(js|jsx)$/,
         exclude: /node_modules/,
         use: ['babel-loader'],
+      },
+      {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader'],
+      },
+      {
+        test: /\.(png|gif|jpg|jpeg|svg|xml|json)$/,
+        use: ['url-loader'],
       }
-    ]
-  }
+    ],
+  },
+  plugins: [
+    new CopyWebpackPlugin([ { from: path.join(cesiumSource, cesiumWorkers), to: 'Workers' } ]),
+    new CopyWebpackPlugin([ { from: path.join(cesiumSource, 'Assets'), to: 'Assets' } ]),
+    new CopyWebpackPlugin([ { from: path.join(cesiumSource, 'Widgets'), to: 'Widgets' } ]),
+    new webpack.DefinePlugin({
+      // Define relative base path in cesium for loading assets
+      CESIUM_BASE_URL: JSON.stringify('')
+    }),
+  ],
+  resolve: {
+		alias: {
+			// Cesium module name
+			cesium: path.resolve(__dirname, cesiumSource)
+		}
+	},
 }
