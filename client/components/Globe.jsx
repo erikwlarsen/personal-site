ea2edfc0c1608d7f6f35e7e98ff00a4fe947b311
erikwlarsen <erik.w.larsen@gmail.com>
2018-06-20 12:56:43 -0400

    (update) move globe options

--- client/components/Globe.jsx
+++ client/components/Globe.jsx
@@ -5,25 +5,27 @@ import React, { Component } from 'react';
 
 Cesium.Ion.defaultAccessToken = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJqdGkiOiI2NTIzNzE0MC0yYzczLTQyNmYtYmM0MS1jYTAxYWZmMzc3NjUiLCJpZCI6MTYxNiwiaWF0IjoxNTI5MjkzMDc3fQ.XtEVkrZCmzGYVe-w4VOb3cKOy2atprfjNX-5HpspZ0k';
 
+const globeOptions = {
+  animation: false,
+  baseLayerPicker: false,
+  fullscreenButton: false,
+  geocoder: false,
+  homeButton: false,
+  infoBox: false,
+  sceneModePicker: false,
+  selectionIndicator: false,
+  timeline: false,
+  navigationHelpButton: false,
+  navigationInstructionsInitiallyVisible: false,
+};
+
 class Globe extends Component {
   constructor(props) {
     super(props);
   }
 
   componentDidMount() {
-    const viewer = new Cesium.Viewer('mount', {
-      animation: false,
-      baseLayerPicker: false,
-      fullscreenButton: false,
-      geocoder: false,
-      homeButton: false,
-      infoBox: false,
-      sceneModePicker: false,
-      selectionIndicator: false,
-      timeline: false,
-      navigationHelpButton: false,
-      navigationInstructionsInitiallyVisible: false,
-    });
+    const viewer = new Cesium.Viewer('mount', globeOptions);
   }
 
   render() {
