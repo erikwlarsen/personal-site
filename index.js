86aef80066e7c8fad5402ba307079a7fa25190d3
erikwlarsen <erik.w.larsen@gmail.com>
2018-06-20 12:55:38 -0400

    (update) import main style sheet into top level

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 import ReactDOM from 'react-dom';
 import React from 'react';
 import App from './client/App.jsx';
-
+import './client/stylesheets/style.scss';
 
 ReactDOM.render(<App />, document.getElementById('root'));
