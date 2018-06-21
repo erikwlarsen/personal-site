eb1fdea345948d2e4b9bee5a8decb0f8654ec54e
erikwlarsen <erik.w.larsen@gmail.com>
2018-06-21 00:35:58 -0400

    (update) import components and basic rendering functionality

--- client/containers/App.jsx
+++ client/containers/App.jsx
@@ -1,21 +1,46 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import Globe from '../components/Globe.jsx';
+import Menu from '../components/Menu.jsx';
+import Main from '../components/Main.jsx';
+import Projects from '../components/Projects.jsx';
+import About from '../components/About.jsx';
+import * as actions from '../actions/actionCreators.js';
 
 const mapStateToProps = (state) => ({
-  selectedComponent: this.state.selectedComponent,
-})
+  selectedComponent: state.main.selectedComponent,
+});
 
 const mapDispatchToProps = {
-  
+  selectComponent: actions.selectComponent,
+};
+
+const componentMap = {
+  main: <Main />,
+  projects: <Projects />,
+  globe: <Globe />,
+  about: <About />,
 }
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+  }
+
   render() {
+    const {
+      selectComponent,
+      selectedComponent,
+    } = this.props;
+
     return (
       <div>
         <h1>SICK APP</h1>
-        <Globe />
+        <Menu
+          selectComponent={selectComponent}
+          selectedComponent={selectedComponent}
+        />
+        {componentMap[selectedComponent]}
       </div>
     );
   }
