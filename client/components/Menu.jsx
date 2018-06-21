90211d4710c48c2345c9bfe814172b0016104bc8
erikwlarsen <erik.w.larsen@gmail.com>
2018-06-21 00:35:39 -0400

    (update) basic main menu functionality

--- client/components/Menu.jsx
+++ client/components/Menu.jsx
@@ -1,7 +1,29 @@
-import React, { Component } from 'react';
+import React from 'react';
 
 const Menu = props => {
-  
+  const handleClick = (e) => {
+    props.selectComponent(e.target.id);
+  };
+
+  const buttonNames = ['Main', 'Projects', 'Globe', 'About'];
+  const buttons = buttonNames.map((name) => {
+    const lowerCaseName = name.toLowerCase();
+    return (
+      <div
+        className={`menu-btn${props.selectedComponent === lowerCaseName ? ' checked' : ''}`}
+        id={lowerCaseName}
+        key={lowerCaseName}
+        onClick={handleClick}>
+        {name}
+      </div>
+    );
+  });
+
+  return (
+    <div id="menu-parent">
+      {buttons}
+    </div>
+  )
 }
 
 export default Menu;
