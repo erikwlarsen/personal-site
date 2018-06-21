98a9ee3cf7450c07a177d263b948423a637cf035
erikwlarsen <erik.w.larsen@gmail.com>
2018-06-21 00:33:11 -0400

    (create) add component skeleton

---
+++ client/components/Main.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Main = (props) => {
+  return (
+    <div>
+      MAIN
+    </div>
+  );
+}
+
+export default Main;
---
+++ client/components/Projects.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Projects = (props) => {
+  return (
+    <div>
+      PROJECTS
+    </div>
+  );
+}
+
+export default Projects;
