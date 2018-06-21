d226ed6bb0422c491f601d45697384b60f74fdfa
erikwlarsen <erik.w.larsen@gmail.com>
2018-06-21 00:33:42 -0400

    (update) incorporate Redux store into top level

--- index.js
+++ index.js
@@ -1,6 +1,14 @@
 import ReactDOM from 'react-dom';
 import React from 'react';
-import App from './client/App.jsx';
+import { Provider } from 'react-redux';
+
+import App from './client/containers/App.jsx';
+import store from './client/store.js';
 import './client/stylesheets/style.scss';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>, 
+  document.getElementById('root')
+);
