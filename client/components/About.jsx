62d348cef21a64fe04e64ff5781b6ea379916025
erikwlarsen <erik.w.larsen@gmail.com>
2018-06-21 19:46:53 -0400

    (update) add tab-opening to about

--- client/components/About.jsx
+++ client/components/About.jsx
@@ -1,11 +1,27 @@
 import React from 'react';
 
-const About = (props) => {
-  return (
-    <div>
-      ABOUT
-    </div>
-  );
+const openInNewTab = (url) => {
+  const win = window.open(url, '_blank');
+  win.focus();
 }
+
+const githubUrl = 'https://github.com/erikwlarsen';
+const linkedinUrl = 'https://www.linkedin.com/in/erik-w-larsen/';
+const resumeUrl = 'https://drive.google.com/file/d/15GDPEiudTMtORyWt-16G8lkSeTyRAJ51/view?usp=sharing';
+
+const About = (props) => (
+  <div>
+    <h3>about</h3>
+    <ul id="about-list">
+      <li>bio
+        <p id="bio">deeyyyooooo</p>
+      </li>
+      <li onClick={() => openInNewTab(githubUrl)}>github</li>
+      <li onClick={() => openInNewTab(resumeUrl)}>resume</li>
+      <li onClick={() => openInNewTab(linkedinUrl)}>linkedin</li>
+    </ul>
+  </div>
+);
+
 
 export default About;
