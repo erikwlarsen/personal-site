de549e2d66057622bd6634eeb208fb5f86ba2935
erikwlarsen <erik.w.larsen@gmail.com>
2018-06-21 00:35:20 -0400

    (update) remove cesium-react

--- client/components/Globe.jsx
+++ client/components/Globe.jsx
@@ -1,6 +1,5 @@
 import Cesium from 'cesium/Cesium';
 import 'cesium/Widgets/widgets.css';
-// import { Viewer, Entity } from 'cesium-react';
 import React, { Component } from 'react';
 
 Cesium.Ion.defaultAccessToken = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJqdGkiOiI2NTIzNzE0MC0yYzczLTQyNmYtYmM0MS1jYTAxYWZmMzc3NjUiLCJpZCI6MTYxNiwiaWF0IjoxNTI5MjkzMDc3fQ.XtEVkrZCmzGYVe-w4VOb3cKOy2atprfjNX-5HpspZ0k';
