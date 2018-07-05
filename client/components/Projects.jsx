fe244cd580cc134c01f9b1cfebc9d93d24743395
erikwlarsen <erik.w.larsen@gmail.com>
2018-07-05 13:44:55 -0500

    (update) rendering Project components

--- client/components/Projects.jsx
+++ client/components/Projects.jsx
@@ -1,10 +1,20 @@
 import React from 'react';
 import Project from './Project.jsx';
 
-const Projects = (props) => {
+const Projects = ({ projects, selectedProject, selectProject }) => {
+  const projComponents = projects.map((proj, idx) => (
+    <Project
+      {...proj}
+      idx={idx}
+      key={proj.title}
+      selected={selectedProject === idx}
+      selectProject={selectProject}
+    />
+  ));
+
   return (
     <div>
-      PROJECTS
+      {projComponents}
     </div>
   );
 }
