e6716c789495c483dd359ed2b3ac906d2bbe3221
erikwlarsen <erik.w.larsen@gmail.com>
2018-07-08 12:12:06 -0500

    (update) display hello worlds

--- client/components/Main.jsx
+++ client/components/Main.jsx
@@ -1,11 +1,27 @@
-import React from 'react';
+import React, { Component } from 'react';
+import { openInNewTab } from '../util/util.js';
 
-const Main = (props) => {
-  return (
-    <div>
-      MAIN
-    </div>
-  );
+class Main extends Component {
+  constructor(props) {
+    super(props);
+  }
+
+  componentDidMount() {
+    this.props.randomHello();
+  }
+
+  render() {
+    const { selectedHello, randomHello } = this.props;
+
+    return (
+      <div>
+        <code onClick={() => openInNewTab('http://wiki.c2.com/?HelloWorldInManyProgrammingLanguages')}>
+          {selectedHello}
+        </code>
+        <div onClick={randomHello}>Hello again!</div>
+      </div>
+    );
+  }
 }
 
 export default Main;
