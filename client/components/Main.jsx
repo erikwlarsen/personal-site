821f14b4f3ded313a187d9f745e22f54a0c93535
erikwlarsen <erik.w.larsen@gmail.com>
2018-07-09 23:41:34 -0400

    (update) move button into main component

--- client/components/Main.jsx
+++ client/components/Main.jsx
@@ -14,13 +14,11 @@ class Main extends Component {
     const { selectedHello, randomHello } = this.props;
 
     return (
-      <div>
-        <div id="main-component">
-          <code onClick={() => openInNewTab('http://wiki.c2.com/?HelloWorldInManyProgrammingLanguages')}>
-            {selectedHello}
-          </code>
-        </div>
+      <div id="main-component">
         <button type="button" id="hello-again" onClick={randomHello}>Hello again!</button>
+        <code onClick={() => openInNewTab('http://wiki.c2.com/?HelloWorldInManyProgrammingLanguages')}>
+          {selectedHello}
+        </code>
       </div>
     );
   }
