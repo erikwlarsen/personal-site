f440e95de1db1d6e0ad1cc744116b3f3739cc112
erikwlarsen <erik.w.larsen@gmail.com>
2018-07-11 18:56:54 -0400

    (update) add external link styling

--- client/components/Project.jsx
+++ client/components/Project.jsx
@@ -5,14 +5,12 @@ const Project = ({ title, link, description, image, idx, selected, selectProject
   return (
     <div className={`project${selected ? ' selected' : ''}`}>
       <h3 onClick={() => selectProject(idx)}>{title}</h3>
-      {selected &&
-        <div>
-          <p>{description}</p>
-          <img src={image} />
-          {/* figure out how to use img complete property to only load when ready */}
-          <p onClick={() => openInNewTab(link)}>view on github</p>
-        </div>
-      }
+      <div>
+        <p>{description}</p>
+        <img src={image} />
+        {/* figure out how to use img complete property to only load when ready */}
+        <p className="external-link" onClick={() => openInNewTab(link)}>view on github</p>
+      </div>
     </div>
   );
 }
