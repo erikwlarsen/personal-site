4eb758627720670f4987d8d1ede528afdae97070
erikwlarsen <erik.w.larsen@gmail.com>
2018-07-15 23:18:25 -0400

    (update) animate hamburger style project display

--- client/components/Project.jsx
+++ client/components/Project.jsx
@@ -5,11 +5,13 @@ const Project = ({ title, link, description, image, idx, selected, selectProject
   return (
     <div className={`project${selected ? ' selected' : ''}`}>
       <h3 onClick={() => selectProject(idx)}>{title}</h3>
-      <div>
-        <p>{description}</p>
-        <img src={image} />
-        {/* figure out how to use img complete property to only load when ready */}
-        <p className="external-link" onClick={() => openInNewTab(link)}>view on github</p>
+      <div className="sub-project">
+        <div>
+          <p>{description}</p>
+          <img src={image} />
+          {/* figure out how to use img complete property to only load when ready */}
+          <p className="external-link" onClick={() => openInNewTab(link)}>view on github</p>
+        </div>
       </div>
     </div>
   );
